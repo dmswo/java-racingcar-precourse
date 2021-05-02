eef7587784143e20a0bd3eed85ef7f42d65cd1c1
dmswo106 <dmswo106@naver.com>
2021-05-02 23:39:28 +0900

    테스트케이스 수정 및 클래스명 수정

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -9,16 +9,16 @@ public class RacingTest {
     @Test
     @DisplayName("자동차_전진검증")
     void 자동차_전진검증() {
-        Racing racing = new Racing();
-        RacingStatus status = racing.moveAndStop(5);
+        Car car = new Car();
+        RacingStatus status = car.moveAndStop(5);
         Assertions.assertThat(status).isEqualTo(RacingStatus.GO);
     }
 
     @Test
     @DisplayName("자동차_멈춤검증")
     void 자동차_멈춤검증() {
-        Racing racing = new Racing();
-        RacingStatus status = racing.moveAndStop(0);
+        Car car = new Car();
+        RacingStatus status = car.moveAndStop(0);
         Assertions.assertThat(status).isEqualTo(RacingStatus.STOP);
     }
 }
